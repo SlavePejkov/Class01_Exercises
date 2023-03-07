17a4a7b13f5125e0226fc4e6e734fb9528f8c56b
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2023-03-07 09:35:06 +0100

    Add files via upload

---
+++ Class01_Exercises/Class01_Exercises/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Class01_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercises 1-4");
+
+            //Exercise 02
+            Console.WriteLine("Hello World");
+            
+
+            //Exercise 03
+
+            Console.WriteLine("    *    ");
+            Console.WriteLine("   ***   ");
+            Console.WriteLine("  *****  ");
+            Console.WriteLine(" ******* ");
+            Console.WriteLine("*********");
+
+            //Exercise 04
+
+            Console.WriteLine("   *   ");
+            Console.WriteLine("   *   ");
+            Console.WriteLine("  ***  ");
+            Console.WriteLine("*  *  *");
+            Console.WriteLine("   *   ");
+            Console.WriteLine("   *   ");
+            Console.WriteLine("  * *  ");
+            Console.WriteLine(" *   * ");
+        }
+    }
+}
